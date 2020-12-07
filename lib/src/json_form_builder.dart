c345cc29114901743b4bb2ee57cdaeaa45f004e0
YuenEsc <yuen.escalante@gmail.com>
2020-12-07 13:59:41 -0600

    Create json_form_builder.dart

---
+++ lib/src/json_form_builder.dart
@@ -0,0 +1,132 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:flutter_form_builder/l10n/messages_all.dart';
+import 'package:intl/intl.dart';
+import 'package:json_form_builder/src/fields/checkbox_group_builder.dart';
+import 'package:json_form_builder/src/fields/checkbox_with_comments.dart';
+import 'package:json_form_builder/src/fields/date_picker_builder.dart';
+import 'package:json_form_builder/src/fields/dropdown_builder.dart';
+import 'package:json_form_builder/src/fields/filter_chip_builder.dart';
+import 'package:json_form_builder/src/fields/location_field_builder.dart';
+import 'package:json_form_builder/src/fields/radio_group_builder.dart';
+import 'package:json_form_builder/src/fields/searchable_dropdown_builder.dart';
+import 'package:json_form_builder/src/fields/segmented_control_builder.dart';
+import 'package:json_form_builder/src/fields/text_field_builder.dart';
+import 'package:json_form_builder/src/fields/title_builder.dart';
+
+class JsonFormBuilder extends StatefulWidget {
+  JsonFormBuilder(
+      {Key key,
+      this.json,
+      this.buttonLabel = 'Finalizar',
+      this.padding = const EdgeInsets.symmetric(
+        horizontal: 32,
+        vertical: 16,
+      ),
+      this.locale = const Locale('es','MX'),
+      this.onSubmitted})
+      : super(key: key);
+
+  final String json;
+  final String buttonLabel;
+  final EdgeInsets padding;
+  final Locale locale;
+  final Function(Map<String, dynamic> result) onSubmitted;
+
+  @override
+  _JsonFormBuilderState createState() => _JsonFormBuilderState();
+}
+
+class _JsonFormBuilderState extends State<JsonFormBuilder> {
+  final GlobalKey<FormBuilderState> fbKey = GlobalKey<FormBuilderState>();
+
+  List<Widget> fields = [];
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    buildFieldsFromJson();
+    initializeMessages(widget.locale.languageCode);
+  }
+
+  @override
+  void didUpdateWidget(Widget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    buildFieldsFromJson();
+  }
+
+  buildFieldsFromJson() {
+    fields=[];
+    List<Map<String, dynamic>> parsedJson =
+        List<Map<String, dynamic>>.from(jsonDecode(widget.json));
+    parsedJson.forEach(
+      (rawField) {
+        if (rawField["type"] == 'datepicker') {
+          fields.add(DatePickerBuilder(rawField: rawField));
+        } else if (rawField["type"] == 'textfield') {
+          fields.add(TextFieldBuilder(rawField: rawField,));
+        } else if (rawField["type"] == 'segmentedcontrol') {
+          fields.add(SegmentedControlBuilder(rawField: rawField));
+        } else if (rawField["type"] == 'radiogroup') {
+          fields.add(RadioGroupBuilder(rawField: rawField));
+        } else if (rawField["type"] == 'dropdown') {
+          fields.add(DropdownBuilder(rawField: rawField));
+        } else if (rawField["type"] == 'checkbox'){
+          fields.add(CheckboxGroupBuilder(rawField: rawField));
+        } else if (rawField["type"] == 'searchabledropdown'){
+          fields.add(SearchableDropdownBuilder(rawField: rawField));
+        } else if (rawField["type"] == 'filterchip'){
+          fields.add(FilterChipBuilder(rawField: rawField));
+        } else if (rawField["type"] == 'locationfield'){
+          fields.add(LocationFieldBuilder(rawField: rawField, fbKey: fbKey));
+        } else if (rawField["type"] == 'checkbox-with-comments'){
+          fields.add(CheckboxWithComments(rawField: rawField));
+        } else if (rawField["type"] == 'title'){
+          fields.add(TitleBuilder(rawField: rawField));
+        }
+        fields.add(
+          SizedBox(
+            height: 32,
+          ),
+        );
+      },
+    );
+    fields.addAll(
+      [
+        SizedBox(
+          height: 48,
+          width: MediaQuery.of(context).size.width,
+          child: ElevatedButton(
+            child: Text(widget.buttonLabel),
+            onPressed: () {
+              if (fbKey.currentState.saveAndValidate()) {
+                Map<String, dynamic> result = fbKey.currentState.value;
+                widget.onSubmitted(result);
+              }
+            },
+          ),
+        )
+      ],
+    );
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: FormBuilder(
+        key: fbKey,
+        child: Padding(
+          padding: widget.padding,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: fields,
+          ),
+        ),
+      ),
+    );
+  }
+}
