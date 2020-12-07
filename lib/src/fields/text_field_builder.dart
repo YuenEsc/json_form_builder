dc3d6cb5971d04602af58b7f706f92a4baf36298
YuenEsc <yuen.escalante@gmail.com>
2020-12-07 13:59:36 -0600

    Create text_field_builder.dart

---
+++ lib/src/fields/text_field_builder.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+
+class TextFieldBuilder extends StatelessWidget {
+  TextFieldBuilder({Key key, this.rawField}) : super(key: key);
+
+  final Map<String, dynamic> rawField;
+    final FocusNode fn = FocusNode();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          alignment: Alignment.centerLeft,
+          padding: rawField.containsKey("label")
+              ? const EdgeInsets.symmetric(vertical: 8.0)
+              : null,
+          child: Text(
+            rawField.containsKey("label") ? rawField["label"] : null,
+            style: Theme.of(context).textTheme.subtitle1,
+            textAlign: TextAlign.left,
+          ),
+        ),
+        FormBuilderTextField(
+          focusNode: fn,
+          decoration: InputDecoration(
+              hintText:
+                  rawField.containsKey("placeholder") ? rawField["placeholder"] : null,
+              border: OutlineInputBorder()),
+          name: rawField["name"],
+          enabled: rawField.containsKey("readOnly") && rawField["readOnly"] == "true"
+                  ? false
+                  : true,
+          initialValue: rawField["value"],
+          validator:
+              rawField.containsKey("required") && rawField["required"] == "true"
+                  ? FormBuilderValidators.required(context)
+                  : null,
+        ),
+      ],
+    );
+  }
+}
