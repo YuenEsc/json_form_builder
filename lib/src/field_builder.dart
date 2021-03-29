f5a958f0ba14db8d0056b9950318389b8f981da3
Yuen Steven Escalante Martínez <yuen.escalante@gmail.com>
2021-03-29 09:23:32 -0600

    field added

---
+++ lib/src/field_builder.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:json_form_builder/src/fields/date_picker_builder.dart';
+import 'package:json_form_builder/src/fields/fields.dart';
+import 'package:json_form_builder/src/fields/image_picker_builder.dart';
+
+///delegates the field constructor to the corresponding type based on it
+class FieldBuilder extends StatelessWidget {
+  const FieldBuilder({
+    Key key,
+    this.fieldSchema,
+    this.onChanged,
+    this.isExpanded = true,
+  }) : super(key: key);
+
+  final Map<String, dynamic> fieldSchema;
+  final ValueChanged onChanged;
+  final bool isExpanded;
+
+  @override
+  Widget build(BuildContext context) {
+    return Builder(
+      builder: (context) {
+        if (fieldSchema.containsKey('type')) {
+          if (fieldSchema["type"] == "textField") {
+            return TextFieldBuilder(
+              fieldSchema: fieldSchema,
+              isExpanded: isExpanded,
+              onChanged: onChanged,
+            );
+          }
+          if (fieldSchema["type"] == "checkboxGroup") {
+            return CheckboxGroupBuilder(
+              fieldSchema: fieldSchema,
+              onChanged: onChanged,
+            );
+          }
+          if (fieldSchema["type"] == "datePicker") {
+            return DatePickerBuilder(
+              fieldSchema: fieldSchema,
+              onChanged: onChanged,
+            
+            );
+          }
+          if (fieldSchema["type"] == "imagePicker") {
+            return ImagePickerBuilder(
+              fieldSchema: fieldSchema,
+              onChanged: onChanged,
+            
+            );
+          }
+          if (fieldSchema["type"] == "segmentedControl") {
+            return SegmentedControlBuilder(
+              fieldSchema: fieldSchema,
+              onChanged: onChanged,
+            
+            );
+          }
+          if (fieldSchema["type"] == "switch") {
+            return SwtichBuilder(
+              fieldSchema: fieldSchema,
+              onChanged: onChanged,
+            
+            );
+          }
+          if (fieldSchema["type"] == "checkbox") {
+            return CheckboxBuilder(
+              fieldSchema: fieldSchema,
+              onChanged: onChanged,
+            
+            );
+          }
+          if (fieldSchema["type"] == "dropdown") {
+            return DropdownBuilder(
+              fieldSchema: fieldSchema,
+              onChanged: onChanged,
+            );
+          }
+           if (fieldSchema["type"] == "tagEditor") {
+            return TagEditorBuilder(
+              fieldSchema: fieldSchema,
+              onChanged: onChanged,
+            );
+          }
+          if (fieldSchema["type"] == "title") {
+            return TitleBuilder(
+              fieldSchema: fieldSchema,
+            );
+          }
+        } else {
+          throw FormatException("type property must be specified");
+        }
+      },
+    );
+  }
+}
